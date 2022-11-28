469744956e0e0ae2bd4974bef7edc351b4352a2a
Gary Ma <gary.ma@csgi.com>
2022-11-28 11:41:58 +0200

    first commit

---
+++ jaeger-server/src/test/java/com/garymar/jaegerserver/JaegerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garymar.jaegerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaegerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
