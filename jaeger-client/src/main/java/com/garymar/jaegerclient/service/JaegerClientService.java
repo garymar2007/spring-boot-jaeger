7bd754ff0549dbfc8698b3b0f284b845bc9c878c
Gary Ma <gary.ma@csgi.com>
2022-11-28 13:58:05 +0200

    jaeger client is done.

---
+++ jaeger-client/src/main/java/com/garymar/jaegerclient/service/JaegerClientService.java
@@ -0,0 +1,19 @@
+package com.garymar.jaegerclient.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class JaegerClientService {
+    private WebClient webClient;
+
+    public JaegerClientService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Mono<String> get(Integer id) {
+        return webClient.get().uri("http://localhost:8082/server/" + id)
+                .retrieve().bodyToMono(String.class);
+    }
+}
